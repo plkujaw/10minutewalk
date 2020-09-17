c6eb7abd469a468753031f7a14451a119cccfdef
plkujaw <kuba.jawor@gmail.com>
2020-09-17 13:07:26 +0100

    return true for the valid directions

--- tenMinuteWalk.js
+++ tenMinuteWalk.js
@@ -1,3 +1,24 @@
-const tenMinuteWalk = () => {};
+const tenMinuteWalk = (directionsArray) => {
+  let n = [];
+  let s = [];
+  let e = [];
+  let w = [];
+  directionsArray.forEach((direction) => {
+    if (direction === "n") {
+      n.push(direction);
+    } else if (direction === "s") {
+      s.push(direction);
+    } else if (direction === "e") {
+      e.push(direction);
+    } else w.push(direction);
+  });
+  if (
+    n.length === s.length &&
+    e.length === w.length &&
+    directionsArray.length === 10
+  ) {
+    return true;
+  }
+};
 
 module.exports = tenMinuteWalk;
--- tenMinuteWalk.test.js
+++ tenMinuteWalk.test.js
@@ -1,1 +1,9 @@
 import tenMinuteWalk from "./tenMinuteWalk.js";
+
+// valid directions - the sum of opposite directions must be equal and total number of directions must be 10; ie sum of "w" must equal sum of "e", the same for "n" and "s" and total of them must be 10
+
+test("should return true for the valid directions", () => {
+  expect(
+    tenMinuteWalk(["w", "s", "e", "e", "n", "n", "e", "s", "w", "w"])
+  ).toBe(true); // 3 "w" + 3 "e" + 2 "s" + 2 "n" = 10
+});
